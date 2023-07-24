b87d4700e263f5e25330ab215db5174d430d27c9
Kuldeep Singh Chouhan <kschouhan714@gmail.com>
2023-07-24 16:21:52 +0530

    Added mongoDB driver

---
+++ src/ProductHub.Api/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductHub.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly ILogger<ProductsController> _logger;
+
+    public ProductsController(ILogger<ProductsController> logger)
+    {
+        _logger = logger;
+    }
+}
